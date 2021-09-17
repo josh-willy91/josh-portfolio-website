52a85f763f2d2429451e1f7e58ad5bb7b7223134
josh-willy91 <jwilly91812@yahoo.com>
2021-09-17 15:30:03 -0400

    about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,10 +9,18 @@ function AboutMe() {
       <div className='about-content'>
         <h2>My Journey</h2>
         <p>
-          {/* As I sat on a plane heading to Vienna, Austria to meet with Peter, the owner of Traiskirchen's
-          baseball club, I would not have guessed my journey would lead me to become a software developer. */}
-
-
+          As I sat on a plane heading to Vienna, Austria to meet with Peter, the owner of Traiskirchen's
+          baseball club, I would not have guessed my journey would lead me to become a software developer.
+          <br/>
+          <br/>
+          What I enjoy about software development is the process of creating something new. I find it
+          so amazing to take an idea for a business or feature and turn that into a reality.
+          <br/>
+          <br/>
+          In my spare time, I like to work on personal projects. Some areas of interest are finance,
+          machine learning, and statistical analysis in the area of sports.  My next personal project
+          will be either a financial market or fantasy baseball machine learning algo to make
+          optimal decisions based on criteria.
         </p>
         <h2>Contact Me</h2>
         <p>
