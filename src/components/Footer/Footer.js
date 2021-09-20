0009d1e134bb9fede1f2d9fd2ee43d023a457847
josh-willy91 <jwilly91812@yahoo.com>
2021-09-20 14:30:16 -0400

    icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,17 +10,17 @@ function Footer() {
                 <a href='https://github.com/josh-willy91'
                     target="_blank"
                     rel="noopener noreferrer">
-                        <img src='images/links/gitHub.png' className='link-img'></img>
+                        <img src='images/icons/gitHub.png' className='link-img'></img>
                 </a>
                 <a href='https://www.linkedin.com/in/joshua-williams-768b48178/'
                     target="_blank"
                     rel="noopener noreferrer">
-                        <img src='images/links/linkedIn.jpg' className='link-img'></img>
+                        <img src='images/icons/linkedIn.jpg' className='link-img'></img>
                 </a>
                 <a href='https://angel.co/u/josh-williams-40'
                     target="_blank"
                     rel="noopener noreferrer">
-                        <img src='images/links/angelList.png' className='link-img'></img>
+                        <img src='images/icons/angelList.png' className='link-img'></img>
                 </a>
             </div>
         </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -42,6 +42,22 @@ function Portfolio() {
   return (
     <div className='portfolio-wrapper'>
       <div className='portfolio-content'>
+        <div className='skills-content'>
+          <h3>Technologies</h3>
+          <img className='skills-icons' src='images/icons/python.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/javascript.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/python.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/node.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/express.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/react.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/reactredux.jpg' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/flask.jpg' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/sqlalchemy.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/sequelize.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/css.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/html.png' alt='technology'></img>
+          <img className='skills-icons' src='images/icons/html.png' alt='technology'></img>
+        </div>
         <div className='project-feature' >
           <Carousel images={robinhoodImages} count={featureCount} setCount={setFeatureCount} />
           <p className='project-feature__details'>
